d778c5b6fba4bf68133df145251edfbf43a41632
Ezequiel Alas <cheke_5@hotmail.com>
2016-08-07 17:47:38 -0500

    ejercicio 5 semana 2 progra

---
+++ ejercicio 5 semana 2 programacion/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_5_semana_2_programacion
+{
+    class Program
+    {
+
+public class mayusculas
+{
+
+    public static void Main()
+    {
+        Console.WriteLine("Ingrese palabra:");
+        string palabra = Console.ReadLine(); 
+
+        Console.WriteLine("El texto es: {0}",
+          palabra);
+        
+        Console.WriteLine("En mayúsculas: {0}",
+          palabra.ToUpper());
+      
+
+                Console.ReadLine();
+    }
+}
+    }
+}
