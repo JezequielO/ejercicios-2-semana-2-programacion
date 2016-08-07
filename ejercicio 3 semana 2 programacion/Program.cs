7ccd6350b6f27a06259dd333daeb57ae0fa3329f
Ezequiel Alas <cheke_5@hotmail.com>
2016-08-07 17:45:49 -0500

    ejercicio 3 semana 2 progra

---
+++ ejercicio 3 semana 2 programacion/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_3_semana_2_programacion
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            var texto = Console.ReadLine();
+            var letras = texto.ToArray();
+            var cantidad = letras.Count();
+
+            for (int i = 0; i < cantidad; i++)
+            { 
+                Console.WriteLine (letras [i]);
+                if (i < cantidad-1)
+                    Console.WriteLine (letras [i] + letras [i + 1]);
+                if (i < cantidad-2)
+                    Console.WriteLine (letras [i] + letras [i + 1] + (i + 2));
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
